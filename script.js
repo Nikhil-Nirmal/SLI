8422d38234c927abc88622341fb32d0e6d9082fd
Nikhil Nirmal <ronnysempire@gmail.com>
2024-02-05 01:54:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+console.log("hii")
+    // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+    // import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+    
+    // const firebaseConfig = {
+    //   apiKey: "AIzaSyCXFHjgvq3uVL-2YiRAhE1fApBCJdJgeQs",
+    //   authDomain: "de-sli.firebaseapp.com",
+    //   databaseURL: "https://de-sli-default-rtdb.firebaseio.com",
+    //   projectId: "de-sli",
+    //   storageBucket: "de-sli.appspot.com",
+    //   messagingSenderId: "442020638826",
+    //   appId: "1:442020638826:web:04ae43a673b749f5dc1a13"
+    // };
+  
+    // // Initialize Firebase
+    // const app = initializeApp(firebaseConfig);
+    // const auth = getAuth();
+
